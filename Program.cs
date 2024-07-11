ab3f9c175fa4d5775bb15b03d8abdb4c050af777
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-07-11 17:46:25 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber;
+            bool isPrime = true;
+            Console.WriteLine("Enter the number");
+            myNumber = Convert.ToInt32(Console.ReadLine());
+            if (myNumber == 0 || myNumber == 1)
+            {
+                Console.WriteLine(myNumber + " is not a Prime Number");
+                isPrime = false;
+            }
+
+            else
+            {
+                for (int i = 2; i < Math.Sqrt(myNumber); i++)
+                {
+                    if (myNumber % i == 0)
+                    {
+
+                        Console.WriteLine(myNumber + " is not a prime number");
+                        isPrime = false;
+                        break;
+
+                    }
+
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine(myNumber + " is a prime number");
+            }
+        }
+    }
+}
